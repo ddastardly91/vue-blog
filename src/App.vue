4adad5e49899ba6bfd8a87f9fe514f3c8b17d09d
Aaron Latham <alatham7891@gmail.com>
2022-10-23 20:56:03 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  setup() {},
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: "Poppins", sans-serif;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+  </div>
+</template>
+
+<script setup>
+//imports
+import getPosts from "../composables/getPosts";
+import PostList from "../components/PostList.vue";
+
+const { posts, error, load } = getPosts();
+
+load();
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+</style>
